685aef729c99b3228a09710009356b011e7688c0
Denis V <5462781+denisvmedia@users.noreply.github.com>
2025-11-17 11:52:49 +0100

    test: add file mode permissions tests for writefile command
    - Add test for default permissions (0644)
    - Add test for custom permissions (0755)
    - Add test for invalid permissions fallback
    - Verify file permissions using fs.Stat()

--- writefile_test.go
+++ writefile_test.go
@@ -3,6 +3,7 @@ package godexer_test
 import (
 	"bytes"
 	"io"
+	"os"
 	"testing"
 
 	qt "github.com/frankban/quicktest"
@@ -37,6 +38,65 @@ func TestWriteFile(t *testing.T) {
 		f, _ := fs.Open("dummy")
 		d, _ := io.ReadAll(f)
 		c.Assert(string(d), qt.Equals, "data val1val2")
+
+		// Verify default permissions (0644)
+		info, err := fs.Stat("dummy")
+		c.Assert(err, qt.IsNil)
+		c.Assert(info.Mode().Perm(), qt.Equals, os.FileMode(0644))
+	})
+
+	t.Run("Execute_CustomPermissions", func(t *testing.T) {
+		c := qt.New(t)
+		fs := afero.NewMemMapFs()
+
+		cmd := godexer.NewWriterFileCommand(&godexer.ExecutorContext{
+			Fs:     fs,
+			Stdout: &bytes.Buffer{},
+			Stderr: &bytes.Buffer{},
+		})
+		ex := cmd.(*godexer.WriteFileCommand)
+		ex.File = "dummy"
+		ex.Contents = "test content"
+		ex.Permissions = "0755"
+		ex.Ectx.Logger = &logger.Logger{}
+
+		m := map[string]any{}
+		err := ex.Execute(m)
+		c.Assert(err, qt.IsNil)
+
+		f, _ := fs.Open("dummy")
+		d, _ := io.ReadAll(f)
+		c.Assert(string(d), qt.Equals, "test content")
+
+		// Verify custom permissions (0755)
+		info, err := fs.Stat("dummy")
+		c.Assert(err, qt.IsNil)
+		c.Assert(info.Mode().Perm(), qt.Equals, os.FileMode(0755))
+	})
+
+	t.Run("Execute_InvalidPermissions", func(t *testing.T) {
+		c := qt.New(t)
+		fs := afero.NewMemMapFs()
+
+		cmd := godexer.NewWriterFileCommand(&godexer.ExecutorContext{
+			Fs:     fs,
+			Stdout: &bytes.Buffer{},
+			Stderr: &bytes.Buffer{},
+		})
+		ex := cmd.(*godexer.WriteFileCommand)
+		ex.File = "dummy"
+		ex.Contents = "test content"
+		ex.Permissions = "invalid"
+		ex.Ectx.Logger = &logger.Logger{}
+
+		m := map[string]any{}
+		err := ex.Execute(m)
+		c.Assert(err, qt.IsNil)
+
+		// Verify it falls back to default permissions (0644) on invalid input
+		info, err := fs.Stat("dummy")
+		c.Assert(err, qt.IsNil)
+		c.Assert(info.Mode().Perm(), qt.Equals, os.FileMode(0644))
 	})
 
 	t.Run("Execute_MissingFile", func(t *testing.T) {
